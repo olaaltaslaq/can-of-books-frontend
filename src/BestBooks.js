4c7918d0c5b3dac5a439082afd7304ca818657cc
root <root@LTUCSTUDENT-L154.localdomain>
2021-08-23 17:42:35 +0300

    add 12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import './BestBooks.css';
+import React from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import "./BestBooks.css";
 
 class MyFavoriteBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    
+    
+    let url =`${process.env.REACT_APP_DATABASE}`;
+
+    console.log(url);
+    let booksData = await axios.get(url);   // http://localhost:4000/books
+    
+    this.setState({
+      books: booksData.data,
+    });
+
+    console.dir(booksData.data[0].title);
+  };
+
   render() {
-    return(
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
-    )
+    return (
+      <div className="tab2">
+        {this.state.books.length !== 0 ? (
+          this.state.books.map((item, i) => {
+            return (
+              <Jumbotron key={i}>
+                <h1>title= {item.title}</h1>
+                <p>{item.description}</p>
+              </Jumbotron>
+
+              // <Card style={{ width: "18rem" }}>
+              //   <Movie
+              //     key={i}
+              //     title={item.title}
+              //     poster={item.poster}
+              //     state={this.state}
+              //   />
+              // </Card>
+            );
+          })
+        ) : (
+          <p>No Books added yet</p>
+        )}
+      </div>
+    );
   }
 }
 
